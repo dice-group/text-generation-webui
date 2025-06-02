8abeda16a2d1bf3faceac081154c61b1a683b9f3
Nikit Srivastava <nikit91@gmail.com>
2025-06-02 23:19:28 +0200

    updating config

--- bloom_start.sh
+++ bloom_start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 export CUDA_VISIBLE_DEVICES="1"
-bash start_linux.sh --listen --listen-port 7860 --model "bigscience_bloom-1b1" --lora "bloom1b1_aya_alpaca_multilingual_peft__attn_rank32"  --trust-remote-code --gradio-auth $TEXTGENWEBUI_USER:$TEXTGENWEBUI_PASS
+bash start_linux.sh --listen --listen-port 7870 --model "bigscience_bloom-1b1" --lora "bloom1b1_aya_alpaca_multilingual_peft__attn_rank32"  --trust-remote-code --gradio-auth $TEXTGENWEBUI_USER:$TEXTGENWEBUI_PASS
